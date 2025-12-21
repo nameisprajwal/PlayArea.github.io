b95d34419b5b51976610a8cce88790294d2b4ef7
Prajwal_M <prajwalm0303@gmail.com>
2025-12-21 08:06:53 -0500

    refactor js

--- js/app-2048.js
+++ js/app-2048.js
@@ -14,222 +14,14 @@ function load2048game() {
   const resultDisplay = document.getElementById('result')
 
   createEmptyBoard(width, gridDisplay, squares)
-  populateATile()
-  populateATile()
+  populateATile(squares)
+  populateATile(squares)
 
-  document.addEventListener('keyup', control)
+  const controlHandler = e => control(e, squares,width, resultDisplay, scoreDisplay,score)
+  document.addEventListener('keyup', controlHandler)
 
   var myTimer = setInterval(addColours, 50)
   addColours()
-
-  
-
-  function populateATile() {
-    checkForGameOver()
-    let randomNumber = Math.floor(Math.random() * squares.length)
-    if (squares[randomNumber].innerHTML == 0) {
-      squares[randomNumber].innerHTML = 2   
-    } else populateATile()
-  }
-  
-  function checkForGameOver() {
-    let zeros = 0
-    let horizontalCheck = 0
-    let verticalCheck = 0   
-    
-    let check = 0
-    for (let i=0; i < squares.length; i++) {
-      if (squares[i].innerHTML == 0) {
-        return true
-        zeros++
-        check++
-      }
-
-      if ( (i+1) % 4 !== 0  &&  i + 1 < squares.length ) { //change 4 to width to make it dynamic
-        if (squares[i].innerHTML === squares[i +1].innerHTML) {
-          return true
-          horizontalCheck++
-          check++
-        }
-      }
-
-      if (i < squares.length - 4) { //change 4 to width to make it dynamic
-        if (squares[i].innerHTML === squares[i +4].innerHTML) {
-          return true
-          verticalCheck++
-          check++
-        }
-      }
-
-    }
-
-    if (check === 0) { //change check to zeros in "make it interesting" version
-      resultDisplay.innerHTML = 'You LOSE'
-      document.removeEventListener('keyup', control)
-      setTimeout(() => clear(), 3000)
-    }
-  }
-
-  function moveRight() {
-    for (let i=0; i < 16; i++) {
-      if (i % 4 === 0) {
-        let totalOne = squares[i].innerHTML
-        let totalTwo = squares[i+1].innerHTML
-        let totalThree = squares[i+2].innerHTML
-        let totalFour = squares[i+3].innerHTML
-        let row = [parseInt(totalOne), parseInt(totalTwo), parseInt(totalThree), parseInt(totalFour)]
-
-        let filteredRow = row.filter(num => num)
-        let missing = 4 - filteredRow.length
-        let zeros = Array(missing).fill(0)
-        let newRow = zeros.concat(filteredRow)
-
-        squares[i].innerHTML = newRow[0]
-        squares[i +1].innerHTML = newRow[1]
-        squares[i +2].innerHTML = newRow[2]
-        squares[i +3].innerHTML = newRow[3]
-      }
-    }
-  }
-
-  function moveLeft() {
-    for (let i=0; i < 16; i++) {
-      if (i % 4 === 0) {
-        let totalOne = squares[i].innerHTML
-        let totalTwo = squares[i+1].innerHTML
-        let totalThree = squares[i+2].innerHTML
-        let totalFour = squares[i+3].innerHTML
-        let row = [parseInt(totalOne), parseInt(totalTwo), parseInt(totalThree), parseInt(totalFour)]
-
-        let filteredRow = row.filter(num => num)
-        let missing = 4 - filteredRow.length
-        let zeros = Array(missing).fill(0)
-        let newRow = filteredRow.concat(zeros)
-
-        squares[i].innerHTML = newRow[0]
-        squares[i +1].innerHTML = newRow[1]
-        squares[i +2].innerHTML = newRow[2]
-        squares[i +3].innerHTML = newRow[3]
-      }
-    }
-  }
-
-
-  function moveUp() {
-    for (let i=0; i < 4; i++) {
-      let totalOne = squares[i].innerHTML
-      let totalTwo = squares[i+width].innerHTML
-      let totalThree = squares[i+(width*2)].innerHTML
-      let totalFour = squares[i+(width*3)].innerHTML
-      let column = [parseInt(totalOne), parseInt(totalTwo), parseInt(totalThree), parseInt(totalFour)]
-
-      let filteredColumn = column.filter(num => num)
-      let missing = 4 - filteredColumn.length
-      let zeros = Array(missing).fill(0)
-      let newColumn = filteredColumn.concat(zeros)
-
-      squares[i].innerHTML = newColumn[0]
-      squares[i +width].innerHTML = newColumn[1]
-      squares[i+(width*2)].innerHTML = newColumn[2]
-      squares[i+(width*3)].innerHTML = newColumn[3]
-    }
-  }
-
-  function moveDown() {
-    for (let i=0; i < 4; i++) {
-      let totalOne = squares[i].innerHTML
-      let totalTwo = squares[i+width].innerHTML
-      let totalThree = squares[i+(width*2)].innerHTML
-      let totalFour = squares[i+(width*3)].innerHTML
-      let column = [parseInt(totalOne), parseInt(totalTwo), parseInt(totalThree), parseInt(totalFour)]
-
-      let filteredColumn = column.filter(num => num)
-      let missing = 4 - filteredColumn.length
-      let zeros = Array(missing).fill(0)
-      let newColumn = zeros.concat(filteredColumn)
-
-      squares[i].innerHTML = newColumn[0]
-      squares[i +width].innerHTML = newColumn[1]
-      squares[i+(width*2)].innerHTML = newColumn[2]
-      squares[i+(width*3)].innerHTML = newColumn[3]
-    }
-  }
-
-  function combineRow() {
-    for (let i =0; i < 15; i++) {
-      if (squares[i].innerHTML === squares[i +1].innerHTML) {
-        let combinedTotal = parseInt(squares[i].innerHTML) + parseInt(squares[i +1].innerHTML)
-        squares[i].innerHTML = combinedTotal
-        squares[i +1].innerHTML = 0
-        score += combinedTotal
-        scoreDisplay.innerHTML = score
-      }
-    }
-    checkForWin()
-  }
-
-  function combineColumn() {
-    for (let i =0; i < 12; i++) {
-      if (squares[i].innerHTML === squares[i +width].innerHTML) {
-        let combinedTotal = parseInt(squares[i].innerHTML) + parseInt(squares[i +width].innerHTML)
-        squares[i].innerHTML = combinedTotal
-        squares[i +width].innerHTML = 0
-        score += combinedTotal
-        scoreDisplay.innerHTML = score
-      }
-    }
-    checkForWin()
-  }
-
-  function control(e) {
-    if(e.keyCode === 37) {
-      keyLeft()
-    } else if (e.keyCode === 38) {
-      keyUp()
-    } else if (e.keyCode === 39) {
-      keyRight()
-    } else if (e.keyCode === 40) {
-      keyDown()
-    }
-  }
-
-  function keyRight() {
-    moveRight()
-    combineRow()
-    moveRight()
-    populateATile()
-  }
-
-  function keyLeft() {
-    moveLeft()
-    combineRow()
-    moveLeft()
-    populateATile()
-  }
-
-  function keyUp() {
-    moveUp()
-    combineColumn()
-    moveUp()
-    populateATile()
-  }
-
-  function keyDown() {
-    moveDown()
-    combineColumn()
-    moveDown()
-    populateATile()
-  }
-
-  function checkForWin() {
-    for (let i=0; i < squares.length; i++) {
-      if (squares[i].innerHTML == 2048) {
-        resultDisplay.innerHTML = 'You WIN'
-        document.removeEventListener('keyup', control)
-        setTimeout(() => clear(), 3000)
-      }
-    }
-  }
 
   function clear() {
     clearInterval(myTimer)
@@ -312,3 +104,224 @@ function createEmptyBoard(inputWidth, gridElement, squaresArray) {
       squaresArray.push(square)
     }
 }
+
+
+function playOneMove(squaresArray, resultDisplay){
+  if(checkForGameOver(squaresArray)){
+    populateATile(squaresArray)
+  }
+  else{
+    conveyGameOver(resultDisplay)
+  }
+}
+
+    function populateATile(squaresArray) {
+        let randomNumber = Math.floor(Math.random() * squaresArray.length)
+        if (squaresArray[randomNumber].innerHTML == 0) {
+          squaresArray[randomNumber].innerHTML = 2   
+        } else populateATile(squaresArray)
+      } 
+
+    function checkForGameOver(squaresArray) {
+      let zeros = 0
+      let horizontalCheck = 0
+      let verticalCheck = 0   
+      let check = 0
+
+      for (let i=0; i < squaresArray.length; i++) {
+        if (squaresArray[i].innerHTML == 0) {
+          return true
+          zeros++
+          check++
+        }
+
+        if ( (i+1) % 4 !== 0  &&  i + 1 < squaresArray.length ) { //change 4 to width to make it dynamic
+          if (squaresArray[i].innerHTML === squaresArray[i +1].innerHTML) {
+            return true
+            horizontalCheck++
+            check++
+          }
+        }
+
+        if (i < squaresArray.length - 4) { //change 4 to width to make it dynamic
+          if (squaresArray[i].innerHTML === squaresArray[i +4].innerHTML) {
+            return true
+            verticalCheck++
+            check++
+          }
+        }
+
+      }
+      return false 
+
+    }
+
+    function conveyGameOver(resultDisplay) {
+      resultDisplay.innerHTML = 'You LOSE'
+      document.removeEventListener('keyup', control)
+      setTimeout(() => clear(), 3000)
+    }
+
+function control(e, squaresArray,width, resultDisplay, scoreDisplay,score) {
+    if(e.keyCode === 37) {
+      keyLeft()
+    } else if (e.keyCode === 38) {
+      keyUp()
+    } else if (e.keyCode === 39) {
+      keyRight()
+    } else if (e.keyCode === 40) {
+      keyDown()
+    }
+
+      function keyRight() {
+        moveRight(squaresArray)
+        combineRow(squaresArray, scoreDisplay,score,resultDisplay)
+        moveRight(squaresArray)
+        playOneMove(squaresArray, resultDisplay)
+      }
+
+      function keyLeft() {
+        moveLeft(squaresArray)
+        combineRow(squaresArray, scoreDisplay,score,resultDisplay)
+        moveLeft(squaresArray)
+        playOneMove(squaresArray, resultDisplay)
+      }
+
+      function keyUp() {
+        moveUp(squaresArray,width)
+        combineColumn(squaresArray,width, scoreDisplay,score,resultDisplay)
+        moveUp(squaresArray,width)
+        playOneMove(squaresArray, resultDisplay)
+      }
+
+      function keyDown() {
+        moveDown(squaresArray,width)
+        combineColumn(squaresArray,width, scoreDisplay,score,resultDisplay)
+        moveDown(squaresArray,width)
+        playOneMove(squaresArray, resultDisplay)
+      } 
+  }
+
+  function moveRight(squares) {
+    for (let i=0; i < 16; i++) {
+      if (i % 4 === 0) {
+        let totalOne = squares[i].innerHTML
+        let totalTwo = squares[i+1].innerHTML
+        let totalThree = squares[i+2].innerHTML
+        let totalFour = squares[i+3].innerHTML
+        let row = [parseInt(totalOne), parseInt(totalTwo), parseInt(totalThree), parseInt(totalFour)]
+
+        let filteredRow = row.filter(num => num)
+        let missing = 4 - filteredRow.length
+        let zeros = Array(missing).fill(0)
+        let newRow = zeros.concat(filteredRow)
+
+        squares[i].innerHTML = newRow[0]
+        squares[i +1].innerHTML = newRow[1]
+        squares[i +2].innerHTML = newRow[2]
+        squares[i +3].innerHTML = newRow[3]
+      }
+    }
+  }
+
+  function moveLeft(squares) {
+    for (let i=0; i < 16; i++) {
+      if (i % 4 === 0) {
+        let totalOne = squares[i].innerHTML
+        let totalTwo = squares[i+1].innerHTML
+        let totalThree = squares[i+2].innerHTML
+        let totalFour = squares[i+3].innerHTML
+        let row = [parseInt(totalOne), parseInt(totalTwo), parseInt(totalThree), parseInt(totalFour)]
+
+        let filteredRow = row.filter(num => num)
+        let missing = 4 - filteredRow.length
+        let zeros = Array(missing).fill(0)
+        let newRow = filteredRow.concat(zeros)
+
+        squares[i].innerHTML = newRow[0]
+        squares[i +1].innerHTML = newRow[1]
+        squares[i +2].innerHTML = newRow[2]
+        squares[i +3].innerHTML = newRow[3]
+      }
+    }
+  }
+
+
+  function moveUp(squares,width) {
+    for (let i=0; i < 4; i++) {
+      let totalOne = squares[i].innerHTML
+      let totalTwo = squares[i+width].innerHTML
+      let totalThree = squares[i+(width*2)].innerHTML
+      let totalFour = squares[i+(width*3)].innerHTML
+      let column = [parseInt(totalOne), parseInt(totalTwo), parseInt(totalThree), parseInt(totalFour)]
+
+      let filteredColumn = column.filter(num => num)
+      let missing = 4 - filteredColumn.length
+      let zeros = Array(missing).fill(0)
+      let newColumn = filteredColumn.concat(zeros)
+
+      squares[i].innerHTML = newColumn[0]
+      squares[i +width].innerHTML = newColumn[1]
+      squares[i+(width*2)].innerHTML = newColumn[2]
+      squares[i+(width*3)].innerHTML = newColumn[3]
+    }
+  }
+
+  function moveDown(squares,width) {
+    for (let i=0; i < 4; i++) {
+      let totalOne = squares[i].innerHTML
+      let totalTwo = squares[i+width].innerHTML
+      let totalThree = squares[i+(width*2)].innerHTML
+      let totalFour = squares[i+(width*3)].innerHTML
+      let column = [parseInt(totalOne), parseInt(totalTwo), parseInt(totalThree), parseInt(totalFour)]
+
+      let filteredColumn = column.filter(num => num)
+      let missing = 4 - filteredColumn.length
+      let zeros = Array(missing).fill(0)
+      let newColumn = zeros.concat(filteredColumn)
+
+      squares[i].innerHTML = newColumn[0]
+      squares[i +width].innerHTML = newColumn[1]
+      squares[i+(width*2)].innerHTML = newColumn[2]
+      squares[i+(width*3)].innerHTML = newColumn[3]
+    }
+  }
+
+   function combineRow(squares, scoreDisplay,score,resultDisplay) {
+    for (let i =0; i < 15; i++) {
+      if (squares[i].innerHTML === squares[i +1].innerHTML) {
+        let combinedTotal = parseInt(squares[i].innerHTML) + parseInt(squares[i +1].innerHTML)
+        squares[i].innerHTML = combinedTotal
+        squares[i +1].innerHTML = 0
+        score += combinedTotal
+        scoreDisplay.innerHTML = score
+      }
+    }
+    checkForWin(squares, resultDisplay, scoreDisplay)
+  }
+
+   function combineColumn(squares,width, scoreDisplay, score,resultDisplay) {
+    for (let i =0; i < 12; i++) {
+      if (squares[i].innerHTML === squares[i +width].innerHTML) {
+        let combinedTotal = parseInt(squares[i].innerHTML) + parseInt(squares[i +width].innerHTML)
+        squares[i].innerHTML = combinedTotal
+        squares[i +width].innerHTML = 0
+        score += combinedTotal
+        scoreDisplay.innerHTML = score
+      }
+    }
+    checkForWin(squares, resultDisplay, scoreDisplay)
+  }
+
+   function checkForWin(squares, resultDisplay, scoreDisplay) {
+    for (let i=0; i < squares.length; i++) {
+      if (squares[i].innerHTML == 2048) {
+        resultDisplay.innerHTML = 'You WIN'
+      
+        const controlHandler = e => control(e, squares,width, resultDisplay, scoreDisplay)
+        document.removeEventListener('keyup', controlHandler)
+        
+        setTimeout(() => clear(), 3000)
+      }
+    }
+  }
